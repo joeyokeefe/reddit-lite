4c0aef28287e6d0f00727ded7d8f0b26c6e93ec9
joseph okeefe <joeyokeefe5@gmail.com>
2023-03-02 19:58:28 -0500

    added functionality for light and dark modes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,18 @@ import DarkMode from "./DarkMode";
 import UserAccount from "./UserAccount";
 import hamburgerMenu from "../assets/hamburgerMenu.svg";
 
-function Header({ setIsOpen }) {
+function Header({ setIsOpen, darkMode, setDarkMode }) {
   return (
     <header className="bg-slate-700 p-3 flex justify-between items-center px-10">
       <h1 className="text-white font-bold text-2xl">
         Reddit<span className="text-blue-300">Lite</span>
       </h1>
       <div className="flex gap-3 items-center">
-        <DarkMode className="cursor-pointer" />
+        <DarkMode
+          className="cursor-pointer"
+          darkMode={darkMode}
+          setDarkMode={setDarkMode}
+        />
         <UserAccount />
         <img
           src={hamburgerMenu}
